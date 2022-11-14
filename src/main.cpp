15c48564ffc8bce633fddd6b3d2dd1777651d18e
Mythical-Atlas <ben@robertcorrell.com>
2022-11-14 13:48:13 -0500

    GLFW window creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,96 @@
+/*
+Zilog Z80 simulator
+Designed and programmed by Ben Correll
+Last updated: 11-14-2022
+*/
+
 #include <iostream>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+#define WINDOW_TITLE "Z80 Simulator"
+
+GLFWwindow* window;
+VkInstance instance;
+
+void initWindow();
+void mainLoop();
+void exitWindow();
+
 int main() {
-    VkApplicationInfo appInfo = {};
-    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Pong";
-    appInfo.pEngineName = "Ponggine";
+    initWindow();
 
-    VkInstanceCreateInfo instanceInfo = {};
-    instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    instanceInfo.pApplicationInfo = &appInfo;
+    mainLoop();
 
-    VkInstance instance;
-    VkResult result = vkCreateInstance(&instanceInfo, 0, &instance);
+    exitWindow();
 
-    if(result == VK_SUCCESS) {std::cout <<  "Successfully created Vulkan instance" << std::endl;}
-    else {
-        std::cout << "Failed to create Vulkan instance" << std::endl;
-        return -1;
-    }
+    return 0;
+}
 
+void initWindow() {
+    // try to init glfw
+    // throw error if glfw fails to init
     if(glfwInit()) {std::cout <<  "Successfully initialized GLFW" << std::endl;}
     else {
         std::cout << "Failed to initialize GLFW" << std::endl;
-        return -1;
+        exit(-1);
     }
 
+    // tell glfw not to create opengl context since we're using vulkan
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    return 0;
+    // actually init window
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
+
+    // set vulkan application info parameters
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = WINDOW_TITLE;
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "Correll Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    // set instance creation parameters
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    // get glfw extensions for vulkan to interface with the window system
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    // bind glfw extensions to vulkan instance creation
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    createInfo.enabledLayerCount = 0;
+
+    // actually create the vulkan instance
+    // throw error if instance failed to init
+    if(vkCreateInstance(&createInfo, nullptr, &instance) == VK_SUCCESS) {std::cout <<  "Successfully created Vulkan instance" << std::endl;}
+    else {
+        std::cout << "Failed to create Vulkan instance" << std::endl;
+        exit(-1);
+    }
+}
+
+void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+}
+
+void exitWindow() {
+    // free vulkan resources
+    vkDestroyInstance(instance, nullptr);
+
+    // free glfw resources
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
